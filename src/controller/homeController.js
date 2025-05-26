c1815f4ffc0c691ab0bd3ba13cdeb51489ae9c1f
Phạm Hoàng Anh <155462810+Kinhart822@users.noreply.github.com>
2025-05-26 22:42:51 +0700

    Update code (CRUD)

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -33,11 +33,49 @@ let createNewUser = async (req, res) => {
         "INSERT INTO users(first_name, last_name, email, address) VALUES ($1, $2, $3, $4)",
         [firstName, lastName, email, address]
     );
-    
+
     // Redirect to home page
     return res.redirect('/');
 }
 
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+
+    // Delete user from PostgreSQL
+    await pool.query("DELETE FROM users WHERE id = $1", [userId]);
+
+    // Redirect to home page
+    return res.redirect('/');
+}
+
+let getUpdatePage = async (req, res) => {
+    let userId = req.params.id;
+    try {
+        let user = await pool.query("SELECT * FROM users WHERE id = $1", [userId]);
+        if (user.rows.length > 0) {
+            return res.render("updateUser.ejs", {
+                user: user.rows[0]
+            });
+        } else {
+            return res.status(404).send("User not found");
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+};
+
+let editUser = async (req, res) => {
+    let { firstName, lastName, email, address, userId } = req.body;
+
+    await pool.query(
+        "UPDATE users SET first_name = $1, last_name = $2, email = $3, address = $4 WHERE id = $5",
+        [firstName, lastName, email, address, userId]
+    );
+
+    return res.redirect('/');
+};
+
 module.exports = {
-    getHomePage, getDetailsPage, createNewUser
+    getHomePage, getDetailsPage, createNewUser, deleteUser, getUpdatePage, editUser
 };
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,10 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage);
     router.get("/details/user/:id", homeController.getDetailsPage);
     router.post("/create-new-user", homeController.createNewUser);
+    router.post("/delete-user", homeController.deleteUser);
+    router.get("/edit-user/:id", homeController.getUpdatePage); 
+    router.post("/edit-user", homeController.editUser);         
+
     router.get('/about', (req, res) => {
         res.send('Hello World from ERIC & HOI DAN IT')
     })
